38b559cece211acc698fea0a37126528d23cedec
Terence Huynh <me@terencehuynh.com>
2017-02-19 19:36:37 +1100

    Cleanup

--- media/js/unihack-2016.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  $("#start-countdown").countdown("2016/07/31 12:00:00",  function(event) {
-    var totalHours = event.offset.totalDays * 24 + event.offset.hours;
-    $(this).text(
-      event.strftime(totalHours + ' hours %M minutes %S seconds')
-    );
-  });
-});
