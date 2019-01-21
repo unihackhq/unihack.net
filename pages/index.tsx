4aac31f2e785daffa03dbcebff2dba88adaf31f7
Anthony Roberts <anthonycr0@users.noreply.github.com>
2019-01-21 21:53:42 +1100

    added event description and city banners

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import Head from 'next/head';
+import './index.scss';
 
 import Page from '../layouts/page/page';
 import Hero from '../components/hero/hero';
+import EventDescription from '../components/event-description/event-description';
+import CityBanner from '../components/city-banner/city-banner';
 
 export default () => (
   <div>
@@ -10,6 +13,11 @@ export default () => (
     </Head>
     <Page withHeader={false}>
       <Hero/>
+      <EventDescription/>
+      <div className="cities">
+        <CityBanner city="Melbourne" year="2018" href="/melbourne" left={false}/>
+        <CityBanner city="Sydney" year="2018" href="/sydney" left/>
+      </div>
     </Page>
   </div>
 );
