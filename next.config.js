8ea5a664b791b8068ae62b221d4df845220bb50c
Anthony Roberts <anthonycr0@users.noreply.github.com>
2019-02-20 21:47:03 +1100

    updated to nextjs 8.0.1 and removed unnecessary config

--- next.config.js
+++ next.config.js
@@ -1,32 +1,15 @@
-const { PHASE_PRODUCTION_SERVER } =
-  process.env.NODE_ENV === 'development'
-    ? {}
-    : !process.env.NOW_REGION
-      ? require('next/constants')
-      : require('next-server/constants');
+const withTypescript = require('@zeit/next-typescript')
+const withSass = require('@zeit/next-sass')
 
-module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_PRODUCTION_SERVER) {
-    // Config used to run in production.
-    return {};
-  }
-
-  // ✅ Put the require call here.
-  const withTypescript = require('@zeit/next-typescript')
-  const withSass = require('@zeit/next-sass')
-
-  return withTypescript(withSass(
-    {
-      target: 'serverless',
-      exportPathMap: function () {
-        return {
-          '/': { page: '/' },
-          '/about': { page: '/about' },
-          '/sponsorship': { page: '/sponsorship' },
-          '/privacy': { page: '/privacy' },
-          '/terms': { page: '/terms' }
-        }
-      }
+module.exports = withTypescript(withSass({
+  target: 'serverless',
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/sponsorship': { page: '/sponsorship' },
+      '/privacy': { page: '/privacy' },
+      '/terms': { page: '/terms' }
     }
-  ));
-};
+  }
+}));
