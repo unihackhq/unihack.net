5003a5c0391cf4fd036d5bc1091636136812a70f
Anthony Roberts <anthonycr0@users.noreply.github.com>
2020-01-09 21:50:48 +1100

    updated prospectus link to 2020 version

--- sections/sponsor-prompt/sponsor-prompt.tsx
+++ sections/sponsor-prompt/sponsor-prompt.tsx
@@ -7,19 +7,30 @@ const SponsorPrompt = () => (
       <h2>Interested?</h2>
       <h3>Contact us for more information</h3>
       <p>
-        We are happy to discuss customisations according to your needs.
-        Send us an email to
-        <a href="mailto:sponsorship@melbourne.unihack.net"> sponsorship@melbourne.unihack.net </a>
+        We're happy to discuss customisations according to your needs. Send us
+        an email at
+        <a href="mailto:sponsorship@melbourne.unihack.net">
+          {' '}
+          sponsorship@melbourne.unihack.net{' '}
+        </a>
         and we’ll follow-up within 24 hours.
       </p>
       <p>
-        For more information on the available tiers and addons, please download our prospectus below.
+        For more information on the available tiers and addons, please download
+        our prospectus below.
       </p>
       <div className="actions">
-        <ButtonLink type="primary" href="mailto:sponsorship@melbourne.unihack.net?subject=General Sponsorship Enquiry">
+        <ButtonLink
+          type="primary"
+          href="mailto:sponsorship@melbourne.unihack.net?subject=General Sponsorship Enquiry"
+        >
           Enquire now
         </ButtonLink>
-        <ButtonLink type="primary" href="https://drive.google.com/file/d/1nRRYqNRZXJKqKT53uielDA4pC_14DA9W/view" target="_blank">
+        <ButtonLink
+          type="primary"
+          href="https://drive.google.com/file/d/0B9ZGzCFzWB_BUnFzTE9QbDhTV3lrTjRrQ3dSSjVnbHQydFEw/view"
+          target="_blank"
+        >
           Download prospectus
         </ButtonLink>
       </div>
