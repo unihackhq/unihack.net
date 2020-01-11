24cbad48aa799bb4c4bdaa855e59ee4b92a80948
Ghina Yashar <ghina.yashar@rea-group.com>
2020-01-11 17:06:44 +1100

    Removed action buttons in hero banner

--- sections/hero/hero.tsx
+++ sections/hero/hero.tsx
@@ -1,6 +1,5 @@
 import './hero.scss';
 import Header from '../../components/header/header';
-import ButtonLink from '../../components/button-link/button-link';
 
 const Hero = () => (
   <section className="hero">
@@ -14,10 +13,6 @@ const Hero = () => (
         <h2>
           <span>March 14-15</span>
         </h2>
-      </div>
-      <div className="actions">
-        <ButtonLink href="https://www.facebook.com/events/742383822828074" type="secondary">Check out our event</ButtonLink>
-        <ButtonLink href="https://unihack2019.devpost.com/" type="secondary">Devpost</ButtonLink>
       </div>
     </div>
   </section>
