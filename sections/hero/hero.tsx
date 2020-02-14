287e24e70a177a1de6a9674e994060378d45c35f
Anthony Roberts <anthonycr0@users.noreply.github.com>
2020-02-14 22:18:05 +1100

    added 2020 eventbrite and facebook link

--- sections/hero/hero.tsx
+++ sections/hero/hero.tsx
@@ -1,5 +1,6 @@
 import './hero.scss';
 import Header from '../../components/header/header';
+import ButtonLink from '../../components/button-link/button-link';
 
 const Hero = () => (
   <section className="hero">
@@ -13,6 +14,20 @@ const Hero = () => (
         <h2>
           <span>March 28-29</span>
         </h2>
+      </div>
+      <div className="actions">
+        <ButtonLink
+          type="secondary"
+          href="https://www.eventbrite.com.au/e/unihack-melbourne-2020-tickets-92874212269"
+        >
+          Get tickets
+        </ButtonLink>
+        <a
+          className="fb-link"
+          href="https://www.facebook.com/events/874105723059342/"
+        >
+          View Facebook event
+        </a>
       </div>
     </div>
   </section>
