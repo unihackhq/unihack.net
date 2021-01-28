2906500db4887b3ae4dbe595b67005ef2e7d4e9e
Anthony Roberts <anthonycr0@users.noreply.github.com>
2021-01-28 23:41:58 +1100

    fixed linting error

--- sections/previous-sponsors/previous-sponsors.tsx
+++ sections/previous-sponsors/previous-sponsors.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './previous-sponsors.module.scss';
 import pastSponsors from '../../content/past-sponsors.json';
 import Grid from '@components/grid/grid';
-import { H2, H3, Text } from '@components/typography/typography';
+import { H2, H3 } from '@components/typography/typography';
 import Stack from '@components/stack/stack';
 
 const PreviousSponsors = () => (
