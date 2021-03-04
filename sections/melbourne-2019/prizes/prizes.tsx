b65fc87f31e94631df5f539b23e61db18726ca3c
Anthony Roberts <anthonycr0@users.noreply.github.com>
2021-03-05 00:32:55 +1100

    updated judging criteria

--- sections/melbourne-2019/prizes/prizes.tsx
+++ sections/melbourne-2019/prizes/prizes.tsx
@@ -37,35 +37,32 @@ const Prizes = () => {
             <Stack size="xsmall">
               <H4>✨ Polish, Design, and Execution</H4>
               <Text>
-                Is the hack usable in its current state? Does it work as the
-                creators say? Is the user experience smooth? Is it well designed
-                for the target audience? Our judges will want to see and use
-                your prototype - so make sure it works!
+                What does the project look like? Is it intuitive, aesthetically
+                pleasing, and accessible? And most importantly, does it actually
+                work?
               </Text>
             </Stack>
             <Stack size="xsmall">
               <H4>💪 Technical Difficulty</H4>
               <Text>
-                Is the hack technically interesting or difficult? Is it just
-                some lipstick on an API, or were there any technical challenges
-                to surmount when building it?
+                Does the hack look technically interesting or challenging? Is it
+                simply putting a pretty frontend on an existing API or does it
+                do something more?
               </Text>
             </Stack>
             <Stack size="xsmall">
               <H4>💡 Originality and Creativity</H4>
               <Text>
-                Is the hack more than just another generic social/mobile/web/IoT
-                solution? Does it do something entirely novel, or at least take
-                a fresh approach to an old problem?
+                How unique is the hack? Does it solve a new problem, or has a
+                new take on an existing problem?
               </Text>
             </Stack>
             <Stack size="xsmall">
               <H4>😲 Wow Factor</H4>
               <Text>
-                Is it something people would actually use? Does it fulfil a real
-                need that the community, the broader society or the target
-                audience has? You do not need to prepare a business plan, but
-                you should at least identify who is your target audience.
+                Does it spark joy? Would this be something you use and show to
+                your friends? Yes, this is totally subjective. But that’s what
+                makes it fun.
               </Text>
             </Stack>
           </div>
