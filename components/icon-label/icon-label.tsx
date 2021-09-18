257ce815c2dfde7332d96b178f388d2c93e378b2
Terence Huynh <me@terencehuynh.com>
2021-09-18 13:10:12 +1000

    Fixes and new hero

--- components/icon-label/icon-label.tsx
+++ components/icon-label/icon-label.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import Icon from '@components/icon/icon';
-import { H3 } from '@components/typography/typography';
+import { H4 } from '@components/typography/typography';
 import styles from './icon-label.module.scss';
 
 type Props = {
   icon: string;
+  title?: string;
   children: React.ReactNode;
 };
 
-export const IconLabel = ({ icon, children }: Props) => (
-  <div className={`${styles['icon-section']}`}>
-    <Icon icon={icon} />
-    <H3>{children}</H3>
-  </div>
-);
+export const IconLabel = ({ icon, title, children }: Props) => {
+  return (
+    <div className={`${styles['icon-section']}`}>
+      <Icon icon={icon} />
+      <div className={`${styles['icon-content']}`}>
+        {!!title && <H4>{title}</H4>}
+        <p>{children}</p>
+      </div>
+    </div>
+  );
+};
--- sections/sponsor-hero/sponsor-hero.tsx
+++ sections/sponsor-hero/sponsor-hero.tsx
@@ -1,38 +1,23 @@
 import React from 'react';
 import styles from './sponsor-hero.module.scss';
-import { H2, Text } from '@components/typography/typography';
 import Stack from '@components/stack/stack';
 import { IconLabel } from '@components/icon-label/icon-label';
 
 const SponsorHero = () => (
   <section className={styles['sponsor-hero']}>
-    <div
-      className={styles['sponsor-hero-content']}
-      style={{
-        background: 'url(/images/sponsor-hero.jpg)',
-        backgroundSize: 'cover',
-      }}
-    >
-      <div className={styles['sponsor-hero-content-left']}>
-        <Stack size="medium">
-          <Stack size="small">
-            <Stack size="medium">
-              <H2>What is UNIHACK?</H2>
-              <Text>
-                UNIHACK is a 48-hour hackathon with the mission to provide a
-                creative space for engineering, design and product students to
-                build innovative projects that are ready to go to market. Come
-                join us in the Imagination Revolution.
-              </Text>
-            </Stack>
-            <Text bold>Come join us in the Imagination Revolution!</Text>
-          </Stack>
-          <Stack size="small">
-            <IconLabel icon="Calendar">Next event TBC</IconLabel>
-            <IconLabel icon="Location">TBC</IconLabel>
-          </Stack>
-        </Stack>
-      </div>
+    <img src="/images/sponsor-hero.png" alt="Sponsorship UNIHACK" />
+    <div className={styles['sponsor-hero-content']}>
+      <Stack size="large">
+        <p className={styles['sponsor-hero-content-subtitle']}>
+          Come join us in the Imagination Revolution!
+        </p>
+        <IconLabel icon="Location" title="Location">
+          Australia &amp; New Zealand
+        </IconLabel>
+        <IconLabel icon="Calendar" title="Date">
+          TBA, Early 2022
+        </IconLabel>
+      </Stack>
     </div>
   </section>
 );
