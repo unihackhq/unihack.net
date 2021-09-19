8b7af8683030bbbae0e4b1fcc36df95fb227d709
Terence Huynh <me@terencehuynh.com>
2021-09-19 20:27:44 +1000

    Make it a post-it note hero

--- sections/sponsor-addons/sponsor-addons.tsx
+++ sections/sponsor-addons/sponsor-addons.tsx
@@ -1,5 +1,4 @@
 import styles from './sponsor-addons.module.scss';
-import ButtonLink from '@components/button/button-link';
 import { H3, Text } from '@components/typography/typography';
 import Stack from '@components/stack/stack';
 
@@ -8,12 +7,7 @@ const Addons = () => (
     <div className={styles['sponsor-addons-content']}>
       <Stack size="medium">
         <Stack size="xsmall">
-          <H3 className={styles['addons-header']}>Addons</H3>
-          <Text>
-            Like the sound of a package but also want a few more perks? That’s
-            cool with us. Attach some addons and experience all that UNIHACK has
-            to offer.
-          </Text>
+          <H3 className={styles['addons-header']}>Want more?</H3>
         </Stack>
         <Stack size="small" className={styles.addons}>
           <div>
@@ -29,11 +23,6 @@ const Addons = () => (
             </div>
           </div>
         </Stack>
-        <div className={styles.actions}>
-          <ButtonLink type="primary" theme="light" href="#sponsor-prompt">
-            Interested?
-          </ButtonLink>
-        </div>
       </Stack>
     </div>
   </section>
