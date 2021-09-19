c9315ee5ab5c6588b699b41f6390a9a234efdb56
Terence Huynh <me@terencehuynh.com>
2021-09-19 19:01:11 +1000

    Change content again

--- sections/sponsor-hero/sponsor-hero.tsx
+++ sections/sponsor-hero/sponsor-hero.tsx
@@ -21,17 +21,20 @@ const SponsorHero = () => (
               <Text>
                 UNIHACK is where Australia's upcoming generation of tech
                 disrupters build and showcase their innovative and market-ready
-                projects over a 48-hour period. Come join us in the imagination
-                revolution for the opportunity to meet and recruit amazing
-                talent, undertake brand promotion, and receive live product
-                feedback.
+                projects over a 48-hour period.
+              </Text>
+              <Text>
+                Come join us in the imagination revolution for the opportunity
+                to meet and recruit amazing talent, undertake brand promotion,
+                and receive live product feedback.
               </Text>
             </Stack>
-            <Text bold>Come join us in the Imagination Revolution!</Text>
           </Stack>
           <Stack size="small">
-            <IconLabel icon="Calendar">Next event TBC</IconLabel>
-            <IconLabel icon="Location">TBC</IconLabel>
+            <IconLabel icon="Calendar">TBA, Early 2022</IconLabel>
+            <IconLabel icon="Location">
+              Remote (Australia &amp; New Zealand)
+            </IconLabel>
           </Stack>
         </Stack>
       </div>
