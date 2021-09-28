dd793cf0a8b113b4bdef2dfb4ec7761804bafc8d
Nick Ang <nicholas_ang@outlook.com>
2021-09-28 21:31:10 +1000

    Add Code of Conduct Page

---
+++ pages/conduct/index.tsx
@@ -0,0 +1,37 @@
+import Head from 'next/head';
+import styles from './conduct.module.scss';
+import Page from '@layouts/page/page';
+import Header from '@components/header/header';
+import { HL } from '@components/typography/typography';
+import Stack from '@components/stack/stack';
+import Rules from '../../content/pages/code-of-conduct.mdx';
+
+const conductPage = () => (
+  <div>
+    <Head>
+      <title>Code of Conduct - UNIHACK</title>
+    </Head>
+    <Page>
+      <Header />
+      <section className={styles['conduct']}>
+        <div className={styles['conduct-content']}>
+          <Stack size="medium">
+            <HL>Code of Conduct</HL>
+            <div className={styles['introduction']}>
+              **We believe that every single person has the right to hack in a
+              safe and welcoming environment.** This is why we have a code of
+              conduct that applies to participants, sponsors, judges, mentors,
+              organisers, volunteers, guests, observers and any other person at
+              a UNIHACK event, or any online interactions in relation to a
+              UNIHACK event (e.g. Discord, Slack, the UNIHACKers Facebook
+              Group).
+            </div>
+            <Rules />
+          </Stack>
+        </div>
+      </section>
+    </Page>
+  </div>
+);
+
+export default conductPage;
