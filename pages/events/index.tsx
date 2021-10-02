26c9698974814b89e13966c42147ea63333fc0bd
Austin <rockmanvnx6@gmail.com>
2021-10-02 16:05:13 +1000

    wip past-event page

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -2,22 +2,27 @@ import Head from 'next/head';
 import Page from '@layouts/page/page';
 import Header from '@components/header/header';
 import Stack from '@components/stack/stack';
-import { HXL } from '@components/typography/typography';
+import { PageNavigation } from '@components/page-navigation/page-navigation';
 import styles from './events.module.scss';
-
+import { PastEvent } from '@sections/past-events';
 export default () => (
-    <div>
-        <Head>
-            <title>Previous events</title>
-        </Head>
+  <div>
+    <Head>
+      <title>Previous events</title>
+    </Head>
 
-        <Page>
-            <Header/>
-            <section className={styles["events"]}>
-                <Stack size="large">
-                    <HXL underlineColor="yellow">Previous Events</HXL>
-                </Stack>
-            </section>
-        </Page>
-    </div>
-)
+    <Page>
+      <Header />
+      <PageNavigation
+        backLinkTitle="Home"
+        backLinkHref="/"
+        underlineColor="yellow"
+      >
+        Previous Events.
+      </PageNavigation>
+      <Stack size="xxlarge" className={styles.content}>
+        <PastEvent />
+      </Stack>
+    </Page>
+  </div>
+);
