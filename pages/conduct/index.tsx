20aefe575b20084b47042f904ac3ab0272eb67cd
Nick Ang <nicholas_ang@outlook.com>
2021-10-02 14:15:46 +1000

    add strong tag to First Sentence

--- pages/conduct/index.tsx
+++ pages/conduct/index.tsx
@@ -18,13 +18,13 @@ const conductPage = () => (
           <Stack size="medium">
             <HL>Code of Conduct</HL>
             <div className={styles['introduction']}>
-              **We believe that every single person has the right to hack in a
-              safe and welcoming environment.** This is why we have a code of
-              conduct that applies to participants, sponsors, judges, mentors,
-              organisers, volunteers, guests, observers and any other person at
-              a UNIHACK event, or any online interactions in relation to a
-              UNIHACK event (e.g. Discord, Slack, the UNIHACKers Facebook
-              Group).
+              <strong> We believe that every single person has the right to hack in a
+              safe and welcoming environment. </strong>
+              This is why we have a code of conduct that applies to
+              participants, sponsors, judges, mentors, organisers, volunteers,
+              guests, observers and any other person at a UNIHACK event, or any
+              online interactions in relation to a UNIHACK event (e.g. Discord,
+              Slack, the UNIHACKers Facebook Group).
             </div>
             <Rules />
           </Stack>
