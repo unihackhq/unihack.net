166423b78e42aeb3b0e79781b96b5e01eb4ae503
Austin <rockmanvnx6@gmail.com>
2021-10-03 12:56:42 +1100

    parsing event content for past events

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -4,8 +4,12 @@ import Header from '@components/header/header';
 import Stack from '@components/stack/stack';
 import { PageNavigation } from '@components/page-navigation/page-navigation';
 import styles from './events.module.scss';
-import { PastEvent } from '@sections/past-events';
-export default () => (
+import { PastEvent, PastEventDescription } from '@sections/past-events';
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
+import { promises as fs } from 'fs';
+import path from 'path';
+
+export default (props: InferGetStaticPropsType<typeof getStaticProps>) => (
   <div>
     <Head>
       <title>Previous events</title>
@@ -21,8 +25,36 @@ export default () => (
         Previous Events.
       </PageNavigation>
       <Stack size="xxlarge" className={styles.content}>
-        <PastEvent />
+        <PastEvent {...props.pastEventDescription} />
       </Stack>
     </Page>
   </div>
 );
+
+export const getStaticProps: GetStaticProps<Content> = async () => {
+  const eventsDirectory = path.join(process.cwd(), 'content/events');
+  let events = await fs.readdir(eventsDirectory);
+
+  // Filter out hidden files
+  events = events.filter(item => !/(^|\/)\.[^\/\.]/g.test(item));
+
+  let pastEventDescription: Array<PastEventDescription> = await Promise.all(
+    events.map(async year => {
+      let eventInfoPath: string = path.join(
+        process.cwd(),
+        `content/events/${year}/info.json`
+      );
+      return {
+        [year]: JSON.parse(await fs.readFile(eventInfoPath, 'utf8'))
+      };
+    })
+  );
+
+  return {
+    props: { pastEventDescription } as Content
+  };
+};
+
+type Content = {
+  pastEventDescription: PastEventDescription[];
+};
