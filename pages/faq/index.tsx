7b0d954a05f58c8a7b9e9f8545bbe077f4400b58
Terence Huynh <me@terencehuynh.com>
2021-11-22 16:00:35 +1100

    FAQs page

---
+++ pages/faq/index.tsx
@@ -0,0 +1,30 @@
+import Head from 'next/head';
+import styles from './faqs.module.scss';
+import Page from '@layouts/page/page';
+import Header from '@components/header/header';
+import { HXL } from '@components/typography/typography';
+import Stack from '@components/stack/stack';
+import FAQContent from '../../content/pages/faqs.mdx';
+
+const FAQPage = () => (
+  <div>
+    <Head>
+      <title>FAQs - UNIHACK</title>
+    </Head>
+    <Page>
+      <Header />
+      <section className={styles['faqs']}>
+        <div className={styles['faqs-title']}>
+          <HXL>FAQs</HXL>
+        </div>
+        <div className={styles['faqs-content']}>
+            <Stack size="small">
+              <FAQContent />
+            </Stack>
+        </div>
+      </section>
+    </Page>
+  </div>
+);
+
+export default FAQPage;
