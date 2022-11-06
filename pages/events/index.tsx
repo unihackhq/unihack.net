5d602d81173ebc55c07b49eac88c9a53229113fe
Terence Huynh <me@terencehuynh.com>
2022-11-06 23:53:27 +1100

    Undo moving of types

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -8,7 +8,32 @@ import { GetStaticProps } from 'next';
 import Filter, { Tags } from '@sections/event-filter';
 import { FC, useEffect, useState } from 'react';
 import { getPastEventInfo } from '@lib/directory';
-import { PastEvents } from './types';
+
+import { JudgeGroup } from '@sections/event-info/judges/judge-group.model';
+import { PrizeData } from '@sections/event-info/prizes/prize-data.model';
+import { SponsorData } from '@sections/event-info/sponsors/sponsor-data.model';
+import { EventScheduleData } from '@sections/event-info/event-schedule/event-schedule-data.model';
+import { EventInfo } from '@sections/event-info/event-hero/event-hero';
+import { StatType } from '@components/stats-banner/stat/stat';
+import { WinnerModel } from '@components/winner/winner.model';
+
+export type EventContent = {
+  judges: {
+    judges: JudgeGroup[];
+  };
+  prizes: {
+    prizes: PrizeData;
+  };
+  schedule: EventScheduleData;
+  sponsors: SponsorData;
+  stats: StatType[];
+  info: EventInfo;
+  winners: WinnerModel[];
+};
+
+export type PastEvents = {
+  [event: string]: EventInfo;
+};
 
 const DisplayTags: { [tag in Tags]: string } = {
   all: 'All',
