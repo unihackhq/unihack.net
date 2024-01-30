79d9a5d792a3559e943e4f7b23856866da3db889
Terence Huynh <me@terencehuynh.com>
2024-01-30 22:31:43 +1100

    update homepage

--- sections/brand-hero/brand-hero.tsx
+++ sections/brand-hero/brand-hero.tsx
@@ -6,30 +6,40 @@ import Stack from '@components/stack/stack';
 
 const BrandHero = () => (
   <section className={styles['brand-hero']}>
-    <Stack size="small" className={styles['brand-hero-content']}>
-      <Text className={styles.title}>
-        <span>UNIHACK is back on March 3!</span>
-      </Text>
-      <Text className={styles.subtitle}>
-        The Imagination Hackathon returns.
-      </Text>
-      <div className={styles.actions}>
-        <ButtonLink
-          type="primary"
-          theme="transparent"
-          href="https://manage.unihack.net/signup"
-        >
-          Register Now
-        </ButtonLink>
-        <ButtonLink
-          type="primary"
-          theme="transparent"
-          href="https://manage.unihack.net"
-        >
-          Manage My Team
-        </ButtonLink>
-      </div>
-    </Stack>
+    <div className={styles['brand-hero-content']}>
+      <Stack size="small">
+        <h1>UNIHACK 2024</h1>
+        <Text className={styles.title}>
+          <span>UNIHACK is back on March 1!</span>
+        </Text>
+        <Text className={styles.subtitle}>
+          Ten years of showcasing student innovation.
+        </Text>
+        <div className={styles.actions}>
+          <ButtonLink
+            type="primary"
+            theme="transparent"
+            href="https://manage.unihack.net/signup"
+          >
+            Register Now
+          </ButtonLink>
+          <ButtonLink
+            type="primary"
+            theme="transparent"
+            href="https://manage.unihack.net"
+          >
+            Manage My Team
+          </ButtonLink>
+          <ButtonLink
+            type="primary"
+            theme="transparent"
+            href="https://merch.unihack.net"
+          >
+            Buy Stickers + T-Shirt
+          </ButtonLink>
+        </div>
+      </Stack>
+    </div>
   </section>
 );
 
