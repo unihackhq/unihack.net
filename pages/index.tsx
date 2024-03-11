b446a42b9bdb2ff920a4b38a4f21cb23c46e1603
Terence Huynh <me@terencehuynh.com>
2024-03-11 05:59:31 +0000

    update to get ready

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,15 +5,14 @@ import Page from '@layouts/page/page';
 import BrandHero from '@sections/brand-hero/stream-hero';
 import { JoinDiscord } from '@sections/homepage/discord';
 import Prizes from '@sections/homepage/prizes/prizes';
-
-import EventSchedule from '@sections/event-info/event-schedule/event-schedule';
-import EventChecklist from '@sections/event-info/event-checklist';
 import Sponsors from '@sections/event-info/sponsors/sponsors';
 
 import { prizes } from '@content/events/2024/prizes.json';
-import schedule from '@content/events/2024/schedule.json';
 import sponsors from '@content/events/2024/sponsors.json';
-import { MelbourneHub, SydneyHub } from '@sections/homepage/hubs';
+import HomeCard from '@components/home-card';
+
+import faqsImage from '../public/images/homepage/faqs.png';
+import inspirationImage from '../public/images/homepage/past.png';
 
 const Index = () => (
   <Page headerless>
@@ -28,12 +27,23 @@ const Index = () => (
     <BrandHero />
     <div className={styles.homepage}>
       <div className={styles.grid}>
-        <MelbourneHub />
-        <SydneyHub />
-      </div>
-      <div className={styles.grid}>
-        <EventSchedule data={schedule} />
-        <EventChecklist />
+        
+        <HomeCard
+          image={faqsImage}
+          title={
+            <>We run Australia's largest university student hackathon</>
+          }
+          placeholderColor="pacific"
+          buttonLink="https://docs.google.com/forms/d/e/1FAIpQLScWRbp9lMacf0Xjul2y2kgLvgsTYqDo1BlrDWBkEAac0UrY0w/viewform"
+          buttonText="Join the team today"
+        />
+        <HomeCard
+          image={inspirationImage}
+          title={<>Interested to see what this year's participants built?</>}
+          placeholderColor="purple"
+          buttonLink="https://unihack2024.devpost.com/"
+          buttonText="Visit DevPost!"
+        />
       </div>
       <JoinDiscord />
       <Prizes data={prizes} />
