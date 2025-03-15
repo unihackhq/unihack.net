d14f16f6a7138ad1e4b517a71af5c38a07fb7b39
Terence Huynh <me@terencehuynh.com>
2025-03-15 11:31:07 +1100

    Update index.tsx

--- pages/countdown/index.tsx
+++ pages/countdown/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import styles from './index.module.scss';
 import { Logo } from '@components/logo';
 
-const END_DATE = '2025/04/16 17:00 +1100';
+const END_DATE = '2025/03/16 17:00 +1100';
 
 interface TimeLeft {
   countdown: boolean;
